e7e6131e02729f5ebf20914daeed4bb36766fbe2
Berke Asmakaya <asmakayab@gmail.com>
2025-10-05 01:09:24 +0300

    feat: add layouts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full min-h-screen bg-white">
+    <div class="w-3/4 mx-auto">
+      <div class="flex justify-center py-8">
+        <AppNavbar />
+      </div>
+      <router-view />
+      <div>
+        <AppFooter />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AppNavbar from './components/AppNavbar/AppNavbar.vue'
+import AppFooter from './components/Footer/AppFooter.vue'
+</script>
+
+<style scoped></style>
