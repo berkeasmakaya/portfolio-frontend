12a1432a7786a71eb8feb27f1417ce62dc934e50
Berke Asmakaya <asmakayab@gmail.com>
2025-10-05 01:11:16 +0300

    feat: add icon package to projects

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './style.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 import App from './App.vue'
 import router from './router'
+
+library.add(fas, fab)
 
 const app = createApp(App)
 
